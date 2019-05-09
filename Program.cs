c83db321f25ad800f7920deff84b2578aac4ca76
ezequiel huma brito <ezequielhuma@hotmail.com>
2019-05-09 16:14:50 -0400

    Tarea 1, ejercicios de los capítulo del 1-4

--- Program.cs
+++ Program.cs
@@ -6,8 +6,15 @@ namespace Tarea1Aplicada1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
 
+            // Primer ejecicio del capitulo 1
+            Console.WriteLine("Ezequiel Huma Brito");
+
+            // Quinto ejercicio del capitulo 5
+            Console.WriteLine("Crispr es una tecnica de edicion genetica que se basa en la forma en que se defienden las bacterias");
+            Console.WriteLine("Alemania es el pais mas rico de Europa");
+            Console.WriteLine("Hernan Cortes fue colonizador de lo que hoy es Peru y Chile");
         }
     }
 }
