62a0415910aa1d7a8e12fe5749586a6dcabf7a82
ezequiel huma brito <ezequielhuma@hotmail.com>
2019-05-09 16:16:21 -0400

    Tarea 1

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace Tarea1Aplicada1
             Console.WriteLine("Crispr es una tecnica de edicion genetica que se basa en la forma en que se defienden las bacterias");
             Console.WriteLine("Alemania es el pais mas rico de Europa");
             Console.WriteLine("Hernan Cortes fue colonizador de lo que hoy es Peru y Chile");
+
         }
     }
 }
